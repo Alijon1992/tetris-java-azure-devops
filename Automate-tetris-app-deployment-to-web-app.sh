539820f467ebac983839cdcf7d220e9feb523b6a
Alijon1992 <121365233+Alijon1992@users.noreply.github.com>
2024-06-13 13:13:56 -0400

    Update Automate-tetris-app-deployment-to-web-app.sh

--- Automate-tetris-app-deployment-to-web-app.sh
+++ Automate-tetris-app-deployment-to-web-app.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 # Set variables
-RESOURCE_GROUP="rg-tetris"
-LOCATION="eastus"
-APP_NAME="app-tetris"
-PLAN_NAME="myAppServicePlan"
+RESOURCE_GROUP="rg-tetris-dev-canadacentral-001"
+LOCATION="canadacentral"
+APP_NAME="app-tetris-dev-canadacentral-001"
+PLAN_NAME="asp-tetris-dev-canadacentral-001"
 ACR_NAME="acrtetris"
 IMAGE_NAME="my-tetris-app:latest"
 
-# Clone the repository
-git clone <repository_url>
-cd <repository_directory>
+# Docker install and run?
+#Systemctl?
 
 # Build the Docker image for the correct platform
-docker build --platform=linux/amd64 -t $ACR_NAME.azurecr.io/$IMAGE_NAME .
+docker build -t $ACR_NAME.azurecr.io/$IMAGE_NAME .
+
+# Create a resource group
+az group create --name $RESOURCE_GROUP --location $LOCATION
+
+# Create the Azure Container Registry with admin enabled
+az acr create --resource-group $RESOURCE_GROUP --name $ACR_NAME --sku Basic --location $LOCATION --admin-enabled true
 
 # Log in to ACR
 az acr login --name $ACR_NAME
@@ -21,40 +26,15 @@ az acr login --name $ACR_NAME
 # Push the Docker image to ACR
 docker push $ACR_NAME.azurecr.io/$IMAGE_NAME
 
-# Create a resource group
-az group create --name $RESOURCE_GROUP --location $LOCATION
-
 # Create an App Service plan
 az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --sku B1 --is-linux
-
-# Create a web app with a Docker container
-az webapp create --resource-group $RESOURCE_GROUP --plan $PLAN_NAME --name $APP_NAME --deployment-container-image-name $ACR_NAME.azurecr.io/$IMAGE_NAME
-
-# Enable managed identity for the web app
-az webapp identity assign --name $APP_NAME --resource-group $RESOURCE_GROUP
-
-# Get the web app's managed identity principal ID
-PRINCIPAL_ID=$(az webapp identity show --name $APP_NAME --resource-group $RESOURCE_GROUP --query principalId --output tsv)
-
-# Get the ACR registry ID
-ACR_ID=$(az acr show --name $ACR_NAME --resource-group $RESOURCE_GROUP --query id --output tsv)
-
-# Assign the acrpull role to the web app
-az role assignment create --assignee $PRINCIPAL_ID --role acrpull --scope $ACR_ID
-
-# Enable ACR admin account (if not already enabled)
-az acr update --name $ACR_NAME --admin-enabled true
 
 # Get ACR credentials
 ACR_USERNAME=$(az acr credential show --name $ACR_NAME --query "username" --output tsv)
 ACR_PASSWORD=$(az acr credential show --name $ACR_NAME --query "passwords[0].value" --output tsv)
 
-# Set the app settings with ACR credentials and port
-az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings \
-DOCKER_REGISTRY_SERVER_URL=https://$ACR_NAME.azurecr.io \
-DOCKER_REGISTRY_SERVER_USERNAME=$ACR_USERNAME \
-DOCKER_REGISTRY_SERVER_PASSWORD=$ACR_PASSWORD \
-WEBSITES_PORT=80
+# Create a web app with a Docker container
+az webapp create --resource-group $RESOURCE_GROUP --plan $PLAN_NAME --name $APP_NAME --container-image-name $ACR_NAME.azurecr.io/$IMAGE_NAME --container-registry-password $ACR_PASSWORD --container-registry-user $ACR_USERNAME
 
 # Configure the web app to use the container image
 az webapp config container set --name $APP_NAME --resource-group $RESOURCE_GROUP --docker-custom-image-name $ACR_NAME.azurecr.io/$IMAGE_NAME --docker-registry-server-url https://$ACR_NAME.azurecr.io --docker-registry-server-user $ACR_USERNAME --docker-registry-server-password $ACR_PASSWORD
