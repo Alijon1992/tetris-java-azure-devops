d3049ca1e221afce043586ea81a3f214893404ad
Alijon1992 <121365233+Alijon1992@users.noreply.github.com>
2024-06-18 00:04:54 -0400

    Update Automate-tetris-app-deployment-to-web-app.sh

--- Automate-tetris-app-deployment-to-web-app.sh
+++ Automate-tetris-app-deployment-to-web-app.sh
@@ -8,12 +8,6 @@ PLAN_NAME="asp-tetris-dev-canadacentral-001"
 ACR_NAME="acrtetris"
 IMAGE_NAME="my-tetris-app:latest"
 
-# Docker install and run?
-#Systemctl?
-
-# Build the Docker image for the correct platform
-docker build -t acrtetris.azurecr.io/my-tetris-app:latest --platform linux/amd64 .
-
 # Create a resource group
 az group create --name $RESOURCE_GROUP --location $LOCATION
 
@@ -21,10 +15,29 @@ az group create --name $RESOURCE_GROUP --location $LOCATION
 az acr create --resource-group $RESOURCE_GROUP --name $ACR_NAME --sku Basic --location $LOCATION --admin-enabled true
 
 # Log in to ACR
-az acr login --name acrtetris
+az acr login --name $ACR_NAME
+
+# Build the Docker image for the correct platform
+docker build -t $ACR_NAME.azurecr.io/$IMAGE_NAME --platform linux/amd64 .
+
+# Verify the image exists locally
+if docker images | grep -q "$ACR_NAME.azurecr.io/$IMAGE_NAME"; then
+  echo "Docker image built successfully."
+else
+  echo "Docker image build failed."
+  exit 1
+fi
 
 # Push the Docker image to ACR
-docker push acrtetris.azurecr.io/my-tetris-app:latest
+docker push $ACR_NAME.azurecr.io/$IMAGE_NAME
+
+# Verify the image exists in ACR
+if az acr repository show-tags --name $ACR_NAME --repository my-tetris-app --output table | grep -q "latest"; then
+  echo "Docker image pushed to ACR successfully."
+else
+  echo "Docker image push to ACR failed."
+  exit 1
+fi
 
 # Create an App Service plan
 az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --sku B1 --is-linux
@@ -40,4 +53,3 @@ az webapp create --resource-group $RESOURCE_GROUP --plan $PLAN_NAME --name $APP_
 az webapp config container set --name $APP_NAME --resource-group $RESOURCE_GROUP --docker-custom-image-name $ACR_NAME.azurecr.io/$IMAGE_NAME --docker-registry-server-url https://$ACR_NAME.azurecr.io --docker-registry-server-user $ACR_USERNAME --docker-registry-server-password $ACR_PASSWORD
 
 echo "Deployment completed successfully."
-
